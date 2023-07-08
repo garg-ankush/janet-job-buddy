723250b01f10dbd2d7c675d876fe564f50c1851a
ankush <unkush-garg@github.com>
2023-07-08 14:50:38 -0500

    Refactor how we record audio

--- record_audio.py
+++ record_audio.py
@@ -1,16 +1,13 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
 
-# Choose your recording parameters
-fs = 44100  # Sample rate
-seconds = 10  # Duration of recording
 
-
-def record_audio(number_of_secs_to_record_for=10):
+def record_audio(output_filename, sample_rate=44100, number_of_secs_to_record_for=10):
     # Record audio
-    recording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+    recording = sd.rec(
+        int(number_of_secs_to_record_for * sample_rate), samplerate=sample_rate, channels=1)
     # Wait for the recording to finish
     sd.wait()
 
     # Save as WAV file
-    write('output.wav', fs, recording)
+    write(f'{output_filename}.wav', sample_rate, recording)
