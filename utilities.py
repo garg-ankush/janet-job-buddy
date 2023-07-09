d1bfc130999f80fe6583832fc3c0e835b215f8f4
Dennis Trujillo <dptru10@gmail.com>
2023-07-08 19:00:47 -0500

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -20,42 +20,6 @@ def generate_job_search_link(keywords):
     return search_url
 
 
-def send_email(sender_email, sender_password, recipient_email, subject, body):
-    # SMTP server configuration (example for Gmail)
-    smtp_server = 'smtp.gmail.com'
-    smtp_port = 587
-
-    # Create a multipart message
-    message = MIMEMultipart()
-    message['From'] = sender_email
-    message['To'] = recipient_email
-    message['Subject'] = subject
-
-    # Add body to the email
-    message.attach(MIMEText(body, 'plain'))
-
-    try:
-        # Create a secure connection to the SMTP server
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-
-        # Login to the sender's email account
-        server.login(sender_email, sender_password)
-
-        # Send the email
-        server.sendmail(sender_email, recipient_email, message.as_string())
-
-        # Close the connection
-        server.quit()
-
-        # Log success message to a file
-        logging.basicConfig(filename='email_log.txt', level=logging.INFO)
-        logging.info("Email sent successfully!")
-    except smtplib.SMTPException as e:
-        # Log failure message to a file
-        logging.basicConfig(filename='email_log.txt', level=logging.ERROR)
-        logging.error("Failed to send email. Error: %s", str(e))
-
 def send_email_from_json(json_file):
     # Load email information from JSON file
     with open(json_file) as f:
